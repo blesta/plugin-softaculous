5d937a7b2f26621ccc7dea23fec30db31466eb38
Temporary <Temporary@Paul-XPS>
2019-12-13 13:50:00 -0800

    Updated to support Plesk

---
+++ lib/cpanel_installer.php
@@ -0,0 +1,127 @@
+<?php
+include_once dirname(__FILE__) . DS . 'softaculous_installer.php';
+class CpanelInstaller extends SoftactulousInstaller
+{
+    /**
+     * Validates informations and runs a softaculous installation script on cPanel
+     *
+     * @param stdClass $service An object representing the cPanel service to execute a script for
+     * @param stdClass $meta The module row meta data for the service
+     * @return boolean Whether the script succeeded
+     */
+    public function install($service, $meta)
+    {
+        if (!isset($this->Clients)) {
+            Loader::loadModels($this, ['Clients']);
+        }
+        if (!isset($this->Form)) {
+            Loader::loadComponents($this, ['Form']);
+        }
+        // Get data for executing script
+        $service_fields = $this->Form->collapseObjectArray($service->fields, 'value', 'key');
+        $config_options = $this->Form->collapseObjectArray($service->options, 'value', 'option_name');
+        $client = $this->Clients->get($service->client_id);
+        // Login and get the cookies
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://' . $meta->host_name . ':2083/login/');
+        curl_setopt($ch, CURLOPT_VERBOSE, 0);
+        // Turn off the server and peer verification (TrustManager Concept).
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        $post = ['user' => $service_fields['cpanel_username'],
+                'pass' => $service_fields['cpanel_password'],
+                'goto_uri' => '/'];
+        curl_setopt($ch, CURLOPT_POST, 1);
+        $nvpreq = http_build_query($post);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $nvpreq);
+        // Check the Header
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $no_follow_location = 0;
+        if (function_exists('ini_get')) {
+            $open_basedir = ini_get('open_basedir'); // Followlocation does not work if open_basedir is enabled
+            if (!empty($open_basedir)) {
+                $no_follow_location = 1;
+            }
+        }
+        if (empty($no_follow_location)) {
+            // Follow redirects
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+        }
+        //curl_setopt($ch, CURLOPT_COOKIEJAR, '-');
+        // Get response from the server.
+        $resp = curl_exec($ch);
+        $error = curl_error($ch);
+        // Did we login ?
+        if ($resp === false) {
+            $this->Input->setErrors([
+                'login' => [
+                    'invalid' => 'Could not login to the remote server. cURL Error : ' . $error
+                ]
+            ]);
+            return;
+        }
+        // Get the cpsess and path to frontend theme
+        $curl_info = curl_getinfo($ch);
+        if (!empty($curl_info['redirect_url'])) {
+            $parsed = parse_url($curl_info['redirect_url']);
+        } else {
+            $parsed = parse_url($curl_info['url']);
+        }
+        $path = trim(dirname($parsed['path']));
+        $path = ($path{0} == '/' ? $path : '/' . $path);
+        curl_close($ch);
+        // Did we login ?
+        if (empty($path)) {
+            $this->Input->setErrors([
+                'login' => [
+                    'invalid' => 'Could not determine the location of the Softaculous on the remote server.'
+                        . ' There could be a firewall preventing access.'
+                ]
+            ]);
+            return;
+        }
+        // Make the Login system
+        $login = 'https://' . rawurlencode($service_fields['cpanel_username']) . ':'
+            . rawurlencode($service_fields['cpanel_password']) . '@' . $meta->host_name . ':2083'
+            . $path . '/softaculous/index.live.php';
+        $data['softdomain'] = $service_fields['cpanel_domain'];
+        // OPTIONAL - By default it will be installed in the /public_html folder
+        $data['softdirectory'] = (!empty($config_options['directory']) ? $config_options['directory'] : '');
+        $data['admin_username'] = $config_options['admin_name'];
+        $data['admin_pass'] = $config_options['admin_pass'];
+        $data['admin_email'] = $client->email;
+        // List of Scripts
+        $scripts = $this->softaculousScripts();
+        $ins_script = $config_options['script'];
+        // Which Script are we to install ?
+        foreach ($scripts as $key => $value) {
+            if (trim(strtolower($value['name'])) == trim(strtolower($ins_script))) {
+                $sid = $key;
+                break;
+            }
+        }
+        // Did we find the Script ?
+        if (empty($sid)) {
+            $this->Input->setErrors([
+                'script_id' => [
+                    'invalid' => 'Could not determine the script to be installed.'
+                    . ' Please make sure the script name is correct. Script Name : ' . $ins_script
+                ]
+            ]);
+            return;
+        }
+        $res = $this->scriptInstallRequest($sid, $login, $data); // Will install the script
+        $res = trim($res);
+        if (preg_match('/installed/is', $res)) {
+            return true;
+        } else {
+            $this->Input->setErrors([
+                'script_id' => [
+                    'invalid' => 'Could not install script: ' . $res
+                ]
+            ]);
+            return false;
+        }
+    }
+}
---
+++ lib/plesk_installer.php
@@ -0,0 +1,148 @@
+<?php
+include_once dirname(__FILE__) . DS . 'softaculous_installer.php';
+class PleskInstaller extends SoftactulousInstaller
+{
+    /**
+     * Validates informations and runs a softaculous installation script on Plesk
+     *
+     * @param stdClass $service An object representing the Plesk service to execute a script for
+     * @param stdClass $meta The module row meta data for the service
+     * @return boolean Whether the script succeeded
+     */
+    public function install($service, $meta)
+    {
+        if (!isset($this->Clients)) {
+            Loader::loadModels($this, ['Clients']);
+        }
+        if (!isset($this->Form)) {
+            Loader::loadComponents($this, ['Form']);
+        }
+
+        // Get data for executing script
+        $serviceFields = $this->Form->collapseObjectArray($service->fields, 'value', 'key');
+        $configOptions = $this->Form->collapseObjectArray($service->options, 'value', 'option_name');
+        $client = $this->Clients->get($service->client_id);
+
+        // Login and get the cookies
+        $loginData = ['login_name' => $meta->username, 'passwd' => $meta->password];
+        $loginUrl = 'https://' . $meta->host_name . ':' . $meta->port . '/login_up.php3';
+        $this->post($loginData, $loginUrl, 'POST');
+
+        // List of Scripts
+        $scripts = $this->softaculousScripts();
+        $installationScript = $configOptions['script'];
+        // Which Script are we to install ?
+        foreach ($scripts as $key => $value) {
+            if (trim(strtolower($value['name'])) == trim(strtolower($installationScript))) {
+                $sid = $key;
+                break;
+            }
+        }
+        // Did we find the Script ?
+        if (empty($sid)) {
+            $this->Input->setErrors([
+                'script_id' => [
+                    'invalid' => 'Could not determine the script to be installed.'
+                    . ' Please make sure the script name is correct. Script Name : ' . $installationScript
+                ]
+            ]);
+            return;
+        }
+
+        // Install the script
+        $data = [
+            'softdomain' => $serviceFields['plesk_domain'],
+            // OPTIONAL - By default it will be installed in the /public_html folder
+            'softdirectory' => (!empty($configOptions['directory']) ? $configOptions['directory'] : ''),
+            'admin_username' => $configOptions['admin_name'],
+            'admin_pass' => $configOptions['admin_pass'],
+            'admin_email' => $client->email
+        ];
+        $response = $this->scriptInstallRequest(
+            $sid,
+            'https://' . $meta->host_name . ':' . $meta->port . '/modules/softaculous/index.php',
+            $data
+        );
+        if ('installed' == strtolower($response)) {
+            return true;
+        } else {
+            $messages = unserialize($response);
+            $this->Input->setErrors([
+                'script_id' => [
+                    'invalid' => 'Could not install script: ' . ($messages ? $messages[0] : $response)
+                ]
+            ]);
+            return false;
+        }
+    }
+
+    /**
+     * Send a request to the Plesk.
+     *
+     * @param array $post The parameters to include in the request
+     * @param string $url Specifies the url to invoke
+     * @param string $method Http request method (GET, DELETE, POST)
+     * @return string An json formatted string containing the response
+     */
+    private function post($post, $url, $method = 'GET')
+    {
+        $ch = curl_init();
+        // Set the request method and parameters
+        switch (strtoupper($method)) {
+            case 'GET':
+            case 'DELETE':
+                $url .= empty($post) ? '' : '?' . http_build_query($post);
+                break;
+            case 'POST':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_POST, 1);
+            default:
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+                break;
+        }
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_VERBOSE, 0);
+        // Turn off the server and peer verification (TrustManager Concept).
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        // Create new session cookies
+        curl_setopt($ch, CURLOPT_COOKIESESSION, true);
+        // Check the Header
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // Get response from the server.
+        $response = curl_exec($ch);
+        $this->setCookie($response);
+        $error = curl_error($ch);
+        if ($error !== '') {
+            $this->Input->setErrors([
+                'login' => [
+                    'invalid' => 'Could not login to the remote server. cURL Error : ' . $error
+                ]
+            ]);
+            return;
+        }
+        $curlInfo = curl_getinfo($ch);
+        curl_close($ch);
+
+        return trim(substr($response, $curlInfo['header_size']));
+    }
+
+    /**
+     * Parses and saves cookies from a response
+     *
+     * @param string $response
+     */
+    private function setCookie($response)
+    {
+        preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $response, $matches);
+        $cookies = array();
+        foreach($matches[1] as $item) {
+            parse_str($item, $cookie);
+            $cookies = array_merge($cookies, $cookie);
+        }
+        foreach ($cookies as $cookie => $value) {
+            $this->cookie = $cookie . '=' . $value;
+        }
+    }
+}

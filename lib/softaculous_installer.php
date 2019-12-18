75004b9f4d774045ca9619e2a673e252630fd835
Temporary <Temporary@Paul-XPS>
2019-12-18 10:45:33 -0800

    Abstracted some shared methods

--- lib/softaculous_installer.php
+++ lib/softaculous_installer.php
@@ -130,4 +130,93 @@ abstract class SoftactulousInstaller
         curl_close($ch);
         return json_decode($resp);
     }
+
+    /**
+     * Send an HTTP request.
+     *
+     * @param array $post The parameters to include in the request
+     * @param string $url Specifies the url to invoke
+     * @param string $method Http request method (GET, DELETE, POST)
+     * @return string An json formatted string containing the response
+     */
+    protected function post(array $post, $url, $method = 'GET')
+    {
+        $ch = curl_init();
+
+        // Set the request method and parameters
+        switch (strtoupper($method)) {
+            case 'GET':
+            case 'DELETE':
+                $url .= empty($post) ? '' : '?' . http_build_query($post);
+                break;
+            case 'POST':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_POST, 1);
+            default:
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+                break;
+        }
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_VERBOSE, 0);
+
+        // Turn off the server and peer verification (TrustManager Concept).
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+        // Create new session cookies
+        curl_setopt($ch, CURLOPT_COOKIESESSION, true);
+
+        // Check the Header
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        // Get response from the server.
+        $response = curl_exec($ch);
+
+        $this->setCookie($response);
+
+
+        $error = curl_error($ch);
+        if ($error !== '') {
+            $errorMessage = Language::_('SoftaculousPlugin.remote_curl_error', true, $error);
+            $this->logger->error($errorMessage);
+            return;
+        }
+
+        $curlInfo = curl_getinfo($ch);
+        curl_close($ch);
+
+        // If we are being redirected, return the CURL info instead of the response
+        if (!empty($curlInfo['redirect_url'])
+            && !empty($curlInfo['url'])
+            && $curlInfo['redirect_url'] != $curlInfo['url']
+        ) {
+            return json_encode($curlInfo);
+        }
+
+        return trim(substr($response, $curlInfo['header_size']));
+    }
+
+    /**
+     * Parses an HTTP response for cookies and records them for later use
+     *
+     * @param string $response The string response from CentOS Web Panel
+     */
+    private function setCookie($response)
+    {
+        preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $response, $matches);
+
+        if (!empty($matches)) {
+            $cookies = [];
+            foreach ($matches[1] as $item) {
+                parse_str($item, $cookie);
+                $cookies = array_merge($cookies, $cookie);
+            }
+
+            foreach ($cookies as $cookie => $value) {
+                $this->cookie = $cookie . '=' . $value;
+            }
+        }
+    }
 }
